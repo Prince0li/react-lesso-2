06e2e76782d8006c4df92b9a9f19a34de3b78d07
Serhii <serega.klimenov11@gmail.com>
2023-02-16 16:24:39 +0300

    add productslist

---
+++ src/components/Products/ProductsList.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@mui/material'
+
+type Props = {}
+const ProductsList = (props: Props) => {
+    return (
+        <Typography variant="h3" component="h2" align="center">
+            ProductsList
+        </Typography>
+    )
+}
+export default ProductsList
