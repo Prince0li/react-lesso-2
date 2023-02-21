3112fbed8544e349a298871bf0e70398731246f0
Serhii <serega.klimenov11@gmail.com>
2023-02-21 17:20:55 +0300

    add utils

--- src/components/Products/ProductsList.tsx
+++ src/components/Products/ProductsList.tsx
@@ -1,5 +1,7 @@
 import { Grid, Typography } from '@mui/material'
 import ProductsListItem from './ProductsListItem'
+import ProductsArray from 'utils/productsArray'
+import productsArray from 'utils/productsArray'
 
 type Props = {}
 
@@ -9,34 +11,25 @@ const ProductsList = (props: Props) => {
             <Typography variant="h3" component="h2" align="center">
                 List of Products
             </Typography>
-            <Grid container spacing={4}>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem
-                        title="iPhone 14 Pro"
-                        description="This is iPhone 14 Pro"
-                        capacity="256"
-                        type="phone"
-                        price={1500}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem
-                        title="iPhone 13 Pro"
-                        description="This is iPhone 13 Pro"
-                        capacity="128"
-                        type="phone"
-                        price={1000}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductsListItem
-                        title="iPhone 12 Pro"
-                        description="This is iPhone 12 Pro"
-                        capacity="128"
-                        type="phone"
-                        price={1000}
-                    />
-                </Grid>
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="stretch"
+                spacing={4}
+            >
+                {productsArray.map((product) => (
+                    <Grid item xs={12} sm={6} md={4}>
+                        <ProductsListItem
+                            title={product.title}
+                            description={product.description}
+                            capacity={product.capacity}
+                            type={product.type}
+                            price={product.price}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
---
+++ src/utils/productsArray.ts
@@ -0,0 +1,56 @@
+type Product = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
+
+const productsArray: Product[] = [
+    {
+        title:"iPhone 12 Pro",
+        description: 'This is iPhone 12 Pro',
+        capacity:"128",
+        type:"phone",
+        price:1000
+    },
+    {
+        title:"iPhone 12 Pro",
+        description: 'This is iPhone 12 Pro',
+        capacity:"128",
+        type:"phone",
+        price:1000
+    },
+    {
+        title:"iPhone 12 Pro",
+        description: 'This is iPhone 12 Pro',
+        capacity:"128",
+        type:"phone",
+        price:1000
+    },
+    {
+        title:"iPhone 12 Pro",
+        description: 'This is iPhone 12 Pro',
+        capacity:"128",
+        type:"phone",
+        price:1000
+    },
+    {
+        title:"iPhone 12 Pro",
+        description: 'This is iPhone 12 Pro',
+        capacity:"128",
+        type:"phone",
+        price:1000
+    },
+    {
+        title:"iPhone 12 Pro",
+        description: 'This is iPhone 12 Pro',
+        capacity:"128",
+        type:"phone",
+        price:1000
+    }
+   
+    
+]
+
+export default productsArray
