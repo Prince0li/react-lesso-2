adcdc934f7d8f6f0f3b749e6b3d375c51b5f49d7
Serhii <serega.klimenov11@gmail.com>
2023-03-03 19:37:07 +0300

    add desturct props

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -19,23 +19,20 @@ type Props = {
 
 class ProductsListItem extends Component<Props> {
     render() {
+        const { title, description, type, capacity, price, image } = this.props
         return (
             <Card className="product" variant="outlined">
                 <CardContent>
                     <div className="product-image">
-                        <img src={this.props.image} alt="" />
+                        <img src={image} alt="" />
                     </div>
-                    <div className="product-title">{this.props.title}</div>
-                    <div className="product-description">
-                        {this.props.description}
-                    </div>
+                    <div className="product-title">{title}</div>
+                    <div className="product-description">{description}</div>
+                    <div className="product-features">Type: {type}</div>
                     <div className="product-features">
-                        Type: {this.props.type}
+                        Capacity: {capacity} Gb
                     </div>
-                    <div className="product-features">
-                        Capacity: {this.props.capacity} Gb
-                    </div>
-                    <div className="product-price">{this.props.price}$</div>
+                    <div className="product-price">{price}$</div>
                     <div className="product-quantity">
                         <Button variant="outlined">-</Button>
                         <TextField size="small" value="1" />
@@ -50,35 +47,4 @@ class ProductsListItem extends Component<Props> {
     }
 }
 
-// const ProductsListItem = ({
-//     title,
-//     description,
-//     type,
-//     capacity,
-//     price,
-//     image,
-// }: Props) => {
-//     return (
-//         <Card className="product" variant="outlined">
-//             <CardContent>
-//                 <div className="product-image">
-//                     <img src={image} alt="" />
-//                 </div>
-//                 <div className="product-title">{title}</div>
-//                 <div className="product-description">{description}</div>
-//                 <div className="product-features">Type: {type}</div>
-//                 <div className="product-features">Capacity: {capacity} Gb</div>
-//                 <div className="product-price">{price}$</div>
-//                 <div className="product-quantity">
-//                     <Button variant="outlined">-</Button>
-//                     <TextField size="small" value="1" />
-//                     <Button variant="outlined">+</Button>
-//                 </div>
-//             </CardContent>
-//             <CardActions className="btns-wrap">
-//                 <Button variant="outlined">Add to cart</Button>
-//             </CardActions>
-//         </Card>
-//     );
-// };
 export default ProductsListItem
