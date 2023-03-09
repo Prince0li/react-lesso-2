5877f9eb482b69f3a0f1af11bf227bdfe6b7c228
Serhii <serega.klimenov11@gmail.com>
2023-03-09 18:49:22 +0300

    add hooks

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -5,6 +5,8 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
+import { useState } from 'react'
+
 import './ProductsListItem.scss'
 type Props = {
     title: string
@@ -23,6 +25,15 @@ const ProductsListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrement = () => {
+        setCount((prevState) => prevState + 1)
+    }
+    const onDecrement = () => {
+        setCount((prevState) => prevState - 1)
+    }
+
     return (
         <Card className="product" variant="outlined">
             <CardContent>
@@ -35,9 +46,21 @@ const ProductsListItem = ({
                 <div className="product-features">Capacity: {capacity} Gb</div>
                 <div className="product-price">{price}$</div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="outlined">+</Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => onDecrement()}
+                        disabled={count <= 1}
+                    >
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button
+                        variant="outlined"
+                        onClick={() => onIncrement()}
+                        disabled={count >= 10}
+                    >
+                        +
+                    </Button>
                 </div>
             </CardContent>
             <CardActions className="btns-wrap">
