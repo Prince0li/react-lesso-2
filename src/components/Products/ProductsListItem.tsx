5c30a890d083908e1fde26867e10ce2bacb6669e
Serhii <serega.klimenov11@gmail.com>
2023-03-09 18:57:38 +0300

    del color changer

--- src/components/Products/ProductsListItem.tsx
+++ src/components/Products/ProductsListItem.tsx
@@ -5,10 +5,9 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
-
 import { useState } from 'react'
-import './ProductsListItem.scss'
 
+import './ProductsListItem.scss'
 type Props = {
     title: string
     description: string
@@ -17,6 +16,7 @@ type Props = {
     price: number
     image: string
 }
+
 const ProductsListItem = ({
     title,
     description,
@@ -26,17 +26,12 @@ const ProductsListItem = ({
     image,
 }: Props) => {
     const [count, setCount] = useState<number>(1)
-    const [color, setColor] = useState<string>('green')
 
     const onIncrement = () => {
         setCount((prevState) => prevState + 1)
     }
     const onDecrement = () => {
         setCount((prevState) => prevState - 1)
-    }
-
-    const changeColor = () => {
-        setColor((prevState) => (prevState === 'green' ? 'red' : 'green'))
     }
 
     return (
@@ -49,10 +44,6 @@ const ProductsListItem = ({
                 <div className="product-description">{description}</div>
                 <div className="product-features">Type: {type}</div>
                 <div className="product-features">Capacity: {capacity} Gb</div>
-                <div>
-                    Color: <span className={color}>{color}</span>
-                </div>
-                <button onClick={() => changeColor()}>Change color</button>
                 <div className="product-price">{price}$</div>
                 <div className="product-quantity">
                     <Button
@@ -78,4 +69,5 @@ const ProductsListItem = ({
         </Card>
     )
 }
+
 export default ProductsListItem
