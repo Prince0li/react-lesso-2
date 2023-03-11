a503ef9f502132445424fca44ef4e95d0f4001f1
Serhii <serega.klimenov11@gmail.com>
2023-03-11 22:32:16 +0300

    summarize total in the cart

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,20 +1,38 @@
-import productsArray from 'utils/productsArray'
+import productsArray, { getProductsObject, Product } from 'utils/productsArray'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
+    productsObject?: {
+        [id: number]: Product
+    }
 }
 
-const CartHeader = ({ productsInCart }: Props) => {
+const CartHeader = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
+}: Props) => {
     return (
         <div>
-            {Object.keys(productsInCart).map((productId) => (
-                <div key={productId}>
-                    {productsArray[parseInt(productId) - 1].title}:{' '}
-                    {productsInCart[parseInt(productId)]}
-                </div>
-            ))}
+            <div>
+                {Object.keys(productsInCart).map((productId) => (
+                    <div key={productId}>
+                        {productsObject[parseInt(productId)].title}:{' '}
+                        {productsInCart[parseInt(productId)]}
+                    </div>
+                ))}
+            </div>
+            <div>
+                Total: $
+                {Object.keys(productsInCart).reduce(
+                    (total, productId) =>
+                        total +
+                        productsObject[parseInt(productId)].price *
+                            productsInCart[parseInt(productId)],
+                    0
+                )}
+            </div>
         </div>
     )
 }
